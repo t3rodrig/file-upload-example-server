090212c7a42593ce7c1b17b2393e36774461b0f2
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-03-07 11:31:46 -0600

    Update thing-model.js

--- models/thing-model.js
+++ models/thing-model.js
@@ -5,7 +5,7 @@ const thingSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
-})
+});
 
 const Thing = mongoose.model('Thing', thingSchema);
 module.exports = Thing;
