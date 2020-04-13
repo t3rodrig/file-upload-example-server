e8b049608f56ddb1e9870d9e0066e8b8b9cd6086
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-04-13 15:12:17 -0500

    Cleanup app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
@@ -31,25 +29,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
 
 // ADD CORS HERE:
 // allow access to the API from diffetent domains/origins
@@ -58,9 +37,6 @@ app.use(cors({
   origin: [ "http://localhost:3000" ]
 }));
 
-
-const index = require('./routes/index');
-app.use('/', index);
 
 // include your new routes here:
 app.use('/api', require('./routes/file-upload-routes'));
